344cbfeb80244f2b0d220ea5fad4992ecfefd5f0
Eldar Bazhenov <104391118+bergemon@users.noreply.github.com>
2025-01-05 18:11:27 +0500

    Delete NrmLauncher directory

--- NrmLauncher/parseSubmods.h
+++
@@ -1,8 +0,0 @@
-#pragma once
-#include <vector>
-#include <filesystem>
-#include <fstream>
-#include <windows.h>
-#include <string>
-
-void parseSubmods(std::vector<std::wstring>& arr1, std::vector<std::wstring>& arr2);
